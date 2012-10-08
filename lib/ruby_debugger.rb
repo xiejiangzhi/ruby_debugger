083f9a779dd0e73412a34ad0007f96f860ea9a9b
xiejiangzhi <xiejiangzhi@gmail.com>
2012-10-08 20:44:15 +0800

    增加启动开关

--- lib/ruby_debugger.rb
+++ lib/ruby_debugger.rb
@@ -2,5 +2,13 @@ require "ruby_debugger/version"
 require "ruby_debugger/debugger"
 
 module RubyDebugger
-  # Your code goes here...
+  @start = false
+
+  def self.start
+    @start = true
+  end
+
+  def self.start?
+    @start
+  end
 end
--- lib/ruby_debugger/debugger.rb
+++ lib/ruby_debugger/debugger.rb
@@ -2,19 +2,18 @@
 
 Binding.class_eval do
   def debug
-    loop do
-      begin
-        print "(irb)> "
-        code = gets
-        puts code
-        break if code =~ /^cont\s*$/
+    return unless RubyDebugger.start?
 
-        print " => "
-        puts eval(code)
-      rescue => e
-        puts e
-      end
-    end
+    begin
+      print "(irb)> "
+      code = gets
+      break if code =~ /^cont\s*$/
+
+      print " => "
+      puts eval(code)
+    rescue => e
+      puts e
+    end while true
   end
 end
 
